375651825e97ad920c4906e41ef18e7b56c7cb52
SSJDeathSpawn <adikesavan28@gmail.com>
2023-10-23 14:35:04 +0530

    Finished most of task 1, all of task 3 and 5

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from typing import List
 from flask import Flask, render_template, request
+
 import re
 
-from utils import extract, notop, get_eqns
+from utils import extract, get_eqns, extract_in_order
 
 app = Flask(__name__)
 
@@ -46,7 +47,11 @@ def val_task2():
 
 
 def val_task3():
-    pass
+    error = None
+    if not bool(re.match(r"\d{2}[A-Z]{3}\d{4}", request.form['regno'])):
+        error = Error()
+        error.regno = "Invalid registration number"
+    return error
 
 
 def val_task4():
@@ -95,6 +100,8 @@ endmodule'''
     match(request.form['type']):
         case 'AOI':
             eqns = get_eqns(f_vals)
+            # Use var count to determine if we even need ands or ors
+            var_count = [sum(['A' <= ch <= 'Z' for ch in term]) for term in eqns]
             and_wire_count = [len(list(filter(lambda x: x.isalpha(), i)))-1 for i in eqns]
             or_wire_count = len(eqns)-2
             wires = "\twire "
@@ -125,34 +132,100 @@ endmodule'''
                             curNot = True
                         else:
                             second = ("not" if curNot else "") + char.lower()
-                        
+
                     if first and second:
                         and_count += 1
-                        ands += f"\tand and{and_count}(andres{num}_{cur}, {first}, {second});\n"
+                        output = f"andres{num}_{cur}"
+                        device = f"and{and_count}"
+                        ands += f"\tand {device}({output}, {first}, {second});\n"
                         first = ""
                         second = ""
                         cur += 1
             ands += "\n"
             ors = ""
-            cur = 0
             for num in range(1, len(eqns)):
-                if cur == 0:
-                    ors += f"\tor or{num}(orres{cur}, andres0_{and_wire_count[0]-1}, andres1_{and_wire_count[1]-1});\n"
-                elif num == len(eqns)-1:
-                    ors += f"\tor or{num}(O, orres{cur-1}, andres{num}_{and_wire_count[num]-1});\n"
+                if num == 1:
+                    if and_wire_count[num-1] > 0:
+                        first = f"andres{num-1}_{and_wire_count[num-1]-1}"
+                    else:
+                        isNot = "not" if '~' in eqns[num-1] else ""
+                        first = isNot + eqns[num-1][-1]
                 else:
-                    ors += f"\tor or{num}(orres{cur}, orres{cur-1}, andres{num}_{and_wire_count[num]-1});\n"
-                cur += 1
+                    first = f"orres{num-2}"
+                second = f"andres{num}_{and_wire_count[num]-1}"
+                output = f"orres{num-1}" if num != len(eqns)-1 else "O"
+
+                ors += f"\tor or{num}({output}, {first}, {second});\n"
             mid = wires + ands + ors
 
             return beg + mid + end + tb
 
         case 'OAI':
-            f_vals = notop(f_vals)
-            return "OAI"
+            eqns = get_eqns(f_vals, inverted=True, pos=True)
+            or_wire_count = [len(list(filter(lambda x: x.isalpha(), i)))-1 for i in eqns]
+            and_wire_count = len(eqns)-2
+            wires = "\twire "
+            for index, count in enumerate(or_wire_count):
+                wires += ", ".join([f"orres{index}_{i}" for i in range(count)]) + ", "
+            wires += ", ".join([f"andres{i}" for i in range(and_wire_count)])
+            wires += ";\n\n"
+
+            ors = ""
+            or_count = 0
+            for num, term in enumerate(eqns):
+                cur = 0
+                curNot = False
+                first = ""
+                second = ""
+                for char in term:
+                    if cur == 0:
+                        if char == "~":
+                            curNot = True
+                        elif not first:
+                            first = ("not" if curNot else "") + char.lower()
+                            curNot = False
+                        else:
+                            second = ("not" if curNot else "") + char.lower()
+                            curNot = False
+                    else:
+                        first = f"orres{num}_{cur-1}"
+                        if char == "~":
+                            curNot = True
+                        else:
+                            second = ("not" if curNot else "") + char.lower()
+
+                    if first and second:
+                        or_count += 1
+                        output = f"orres{num}_{cur}"
+                        device = f"or{or_count}"
+                        ors += f"\tor {device}({output}, {first}, {second});\n"
+                        first = ""
+                        second = ""
+                        cur += 1
+            ors += "\n"
+            ands = ""
+            for num in range(1, len(eqns)):
+                if num == 1:
+                    if or_wire_count[num-1] > 0:
+                        first = f"orres{num-1}_{or_wire_count[num-1]-1}"
+                    else:
+                        isNot = "not" if '~' in eqns[num-1] else ""
+                        first = isNot + eqns[num-1][-1]
+                else:
+                    first = f"andres{num-2}"
+                second = f"orres{num}_{or_wire_count[num]-1}"
+                output = f"andres{num-1}" if num != len(eqns)-1 else "O"
+
+                ands += f"\tand and{num}({output}, {first}, {second});\n"
+
+            mid = wires + ors + ands
+
+            return beg + mid + end + tb
 
         case 'NAND':
             eqns = get_eqns(f_vals)
+            var_count = [sum(['A' <= ch <= 'Z' for ch in term]) for term in eqns]
+            print(var_count)
             and_wire_count = [len(list(filter(lambda x: x.isalpha(), i)))-1 for i in eqns]
             or_wire_count = len(eqns)-2
             wires = "\twire "
@@ -210,9 +283,69 @@ endmodule'''
             mid = wires + ands + ors
 
             return beg + mid + end + tb
+
         case 'NOR':
-            f_vals = notop(f_vals)
-            return "NOR"
+            eqns = get_eqns(f_vals, inverted=True, pos=True)
+            var_count = [sum(['A' <= ch <= 'Z' for ch in term]) for term in eqns]
+            print(var_count)
+            or_wire_count = [len(list(filter(lambda x: x.isalpha(), i)))-1 for i in eqns]
+            and_wire_count = len(eqns)-2
+            wires = "\twire "
+            for index, count in enumerate(or_wire_count):
+                wires += ", ".join([f"norres{index}_{i}" for i in range(count)]) + ", "
+                if count>1:
+                    wires += ", ".join([f"orres{index}_{i}" for i in range(count-1)]) + ", "
+            wires += ", ".join([f"andres{i}" for i in range(and_wire_count)]) + ", "
+            wires += ", ".join([f"notandres{i}" for i in range(and_wire_count-1)])
+            wires += ";\n\n"
+            ors = ""
+            or_count = 0
+            for num, term in enumerate(eqns):
+                cur = 0
+                curNot = False
+                first = ""
+                second = ""
+                for char in term:
+                    if cur == 0:
+                        if char == "~":
+                            curNot = True
+                        elif not first:
+                            first = ("not" if curNot else "") + char.lower()
+                            curNot = False
+                        else:
+                            second = ("not" if curNot else "") + char.lower()
+                            curNot = False
+                    else:
+                        first = f"orres{num}_{cur-1}"
+                        if char == "~":
+                            curNot = True
+                        else:
+                            second = ("not" if curNot else "") + char.lower()
+                        
+                    if first and second:
+                        or_count += 1
+                        ors += f"\tnor nor{or_count}(norres{num}_{cur}, {first}, {second});\n"
+                        if cur < or_wire_count[num]-1:
+                            ors += f"\tnot not{4+or_count}(orres{num}_{cur}, norres{num}_{cur});\n"
+                        first = ""
+                        second = ""
+                        cur += 1
+            ors += "\n"
+            ands = ""
+            cur = 0
+            for num in range(1, len(eqns)):
+                if cur == 0:
+                    ands += f"\tnor nor{or_count+num}(andres{cur}, norres0_{or_wire_count[0]-1}, norres1_{or_wire_count[1]-1});\n"
+                    ands += f"\tnot not{4+or_count+num}(notandres{cur}, andres{cur});\n"
+                elif num == len(eqns)-1:
+                    ands += f"\tnor nor{or_count+num}(O, notandres{cur-1}, norres{num}_{or_wire_count[num]-1});\n"
+                else:
+                    ands += f"\tnor nor{or_count+num}(andres{cur}, notandres{cur-1}, norres{num}_{or_wire_count[num]-1});\n"
+                    ands += f"\tnot not{4+or_count+num}(notandres{cur}, andres{cur});\n"
+                cur += 1
+            mid = wires + ors + ands
+
+            return beg + mid + end + tb
 
 
 def task2():
@@ -220,7 +353,150 @@ def task2():
 
 
 def task3():
-    pass
+    f_vals: List[int] = extract(request.form['regno'])
+    beg = '''module DEC(O, i);
+\tinput[3:0] i;
+\toutput[15:0] O;
+\tassign O = {i[3] & i[2] & i[1] & i[0], i[3] & i[2] & i[1] & ~i[0], i[3] & i[2] & ~i[1] & i[0], i[3] & i[2] & ~i[1] & ~i[0], i[3] & ~i[2] & i[1] & i[0], i[3] & ~i[2] & i[1] & ~i[0], i[3] & ~i[2] & ~i[1] & i[0], i[3] & ~i[2] & ~i[1] & ~i[0], ~i[3] & i[2] & i[1] & i[0], ~i[3] & i[2] & i[1] & ~i[0], ~i[3] & i[2] & ~i[1] & i[0], ~i[3] & i[2] & ~i[1] & ~i[0], ~i[3] & ~i[2] & i[1] & i[0], ~i[3] & ~i[2] & i[1] & ~i[0], ~i[3] & ~i[2] & ~i[1] & i[0], ~i[3] & ~i[2] & ~i[1] & ~i[0]};
+endmodule
+
+module MUX8(O,i,s);
+\tinput[7:0] i;
+\tinput[2:0] s;
+\toutput reg O;
+\talways @(i or s) begin
+\t\tcase(s)
+\t\t\t3'b000:O = i[0];
+\t\t\t3'b001:O = i[1];
+\t\t\t3'b010:O = i[2];
+\t\t\t3'b011:O = i[3];
+\t\t\t3'b100:O = i[4];
+\t\t\t3'b101:O = i[5];
+\t\t\t3'b110:O = i[6];
+\t\t\t3'b111:O = i[7];
+\t\tendcase
+\tend
+endmodule
+
+module MUX16(O,i,s);
+\tinput[15:0] i;
+\tinput[3:0] s;
+\toutput reg O;
+
+\talways @(i or s) begin
+\t\tcase(s)
+\t\t\t4'b0000:O = i[0];
+\t\t\t4'b0001:O = i[1];
+\t\t\t4'b0010:O = i[2];
+\t\t\t4'b0011:O = i[3];
+\t\t\t4'b0100:O = i[4];
+\t\t\t4'b0101:O = i[5];
+\t\t\t4'b0110:O = i[6];
+\t\t\t4'b0111:O = i[7];
+\t\t\t4'b1000:O = i[8];
+\t\t\t4'b1001:O = i[9];
+\t\t\t4'b1010:O = i[10];
+\t\t\t4'b1011:O = i[11];
+\t\t\t4'b1100:O = i[12];
+\t\t\t4'b1101:O = i[13];
+\t\t\t4'b1110:O = i[14];
+\t\t\t4'b1111:O = i[15];
+\t\tendcase
+\tend
+endmodule
+
+module task3(a,b,c,d,O);
+\tinput a,b,c,d;
+\toutput O;
+\twire nota, notb, notc, notd;
+\tnot not1(nota, a);
+\tnot not2(notb, b);
+\tnot not3(notc, c);
+\tnot not4(notd, d);
+'''
+    end = 'endmodule'
+    tb = '''
+
+module task3_tb;
+\treg a,b,c,d;
+\twire O;
+\ttask3 inst(a,b,c,d,O);
+\tinteger i;
+
+\tinitial begin
+\t\tfor(i=0;i<16;i=i+1) begin
+\t\t\t{a,b,c,d} = i;
+\t\t\t#10;
+\t\tend
+\tend
+endmodule'''
+
+    states = [lambda x: "1'b0",
+              lambda x: "not" + x,
+              lambda x: x,
+              lambda x: "1'b1"]
+    match(request.form['type']):
+
+        case "MUX16":
+            conns = [states[int(i in f_vals)*3](None) for i in range(15, -1, -1)]
+
+            mid = "\tMUX16 mux(O, "
+            mid += f"{{{', '.join(conns)}}}, {{a, b, c, d}});\n"
+
+        case "MUX8A":
+            def get_index(n):
+                return int(n in f_vals) + int(n+8 in f_vals)*2
+
+            indices = [get_index(i) for i in range(7, -1, -1)]
+            conns = [states[index]('a') for index in indices]
+            mid = "\tMUX8 mux(O, "
+            mid += f"{{{', '.join(conns)}}}, {{b, c, d}});\n"
+
+        case "MUX8B":
+            def chunk(lst, n):
+                for i in range(0, len(lst), n):
+                    yield lst[i:i + n]
+
+            def get_index(n):
+                return int(n in f_vals) + int(n+4 in f_vals)*2
+
+            check_list = sum(list(chunk(list(range(15, -1, -1)), 4))[1::2], [])
+            indices = [get_index(i) for i in check_list]
+            print(check_list)
+            conns = [states[index]('b') for index in indices]
+            mid = "\tMUX8 mux(O, "
+            mid += f"{{{', '.join(conns)}}}, {{a, c, d}});\n"
+
+        case "MUX8C":
+            def chunk(lst, n):
+                for i in range(0, len(lst), n):
+                    yield lst[i:i + n]
+
+            def get_index(n):
+                return int(n in f_vals) + int(n+2 in f_vals)*2
+
+            check_list = sum(list(chunk(list(range(15, -1, -1)), 2))[1::2], [])
+            indices = [get_index(i) for i in check_list]
+            print(check_list)
+            conns = [states[index]('c') for index in indices]
+            mid = "\tMUX8 mux(O, "
+            mid += f"{{{', '.join(conns)}}}, {{a, b, d}});\n"
+
+        case "MUX8D":
+            def get_index(n):
+                return int(n in f_vals) + int(n+1 in f_vals)*2
+
+            indices = [get_index(i) for i in range(14, -1, -2)]
+            conns = [states[index]('d') for index in indices]
+            mid = "\tMUX8 mux(O, "
+            mid += f"{{{', '.join(conns)}}}, {{a, b, c}});\n"
+
+        case "DECODER":
+            mid = "\twire[15:0] mid;\n"
+            mid += "\tDEC dec(mid, {a,b,c,d});\n"
+            mid += f"\tor or1(O, {', '.join([f'mid[{i}]' for i in f_vals])});\n"
+
+    return beg + mid + end + tb
 
 
 def task4():
@@ -228,7 +504,41 @@ def task4():
 
 
 def task5():
-    pass
+    f_vals: List[int] = extract_in_order(request.form['regno'])
+    beg = '''module task5(i, clk, O);
+\tinput[3:0] i;
+\tinput clk;
+\toutput reg O;
+\talways @(posedge clk) begin
+\t\tcase(i):
+'''
+    end = '''\t\tendcase
+\tend
+endmodule'''
+    tb = '''
+
+module task5_tb;
+\treg[3:0] i;
+\twire O;
+\ttask3 inst(i,O);
+\tinteger c;
+
+\tinitial begin
+\t\tfor(c=0;c<16;c=c+1) begin
+\t\t\ti = c;
+\t\t\t#10;
+\t\tend
+\tend
+endmodule'''
+
+    mid = ""
+    for idx, curr in enumerate(f_vals[:-1]):
+        next = f_vals[idx+1]
+        mid += f"\t\t\t4'b{bin(curr)[2:].rjust(4, '0')}: O = 4'b{bin(next)[2:].rjust(4, '0')}\n"
+    mid += f"\t\t\t4'b{bin(f_vals[-1])[2:].rjust(4, '0')}: O = 4'b{bin(f_vals[0])[2:].rjust(4, '0')}\n"
+    mid += f"\t\t\tdefault: 4'b{bin(f_vals[0])[2:].rjust(4, '0')}\n"
+
+    return beg + mid + end + tb
 
 
 app.run(debug=True)
--- utils/dsd.py
+++ utils/dsd.py
@@ -7,19 +7,28 @@ from libs.kmap import Minterms, Term
 def extract(regno: str) -> List[int]:
     nums: List[int] = []
     for i in regno:
-        if i.isnumeric():
-            nums.append(int(i))
-        elif 'A' <= i <= 'F':
+        if 'A' <= i <= 'F' or i.isnumeric():
             nums.append(int(i, 16))
 
     return list(set(nums))
+
+
+def extract_in_order(regno: str) -> List[int]:
+    nums: List[int] = []
+    for i in regno:
+        if 'A' <= i <= 'F' or i.isnumeric():
+            if int(i, 16) in nums:
+                continue
+            nums.append(int(i, 16))
+
+    return nums
 
 
 def notop(F: List[int]) -> List[int]:
     return [i for i in range(16) if i not in F]
 
 
-def get_eqns(f_vals: List[int], inverted: bool = False):
+def get_eqns(f_vals: List[int], inverted: bool = False, pos: bool = False):
     f_vals = notop(f_vals) if inverted else f_vals
     min_terms: List[str] = [Term("%04d" % int(bin(i)[2:])) for i in f_vals]
     mask = Minterms(min_terms).simplify()
@@ -27,6 +36,11 @@ def get_eqns(f_vals: List[int], inverted: bool = False):
 
     # Make into ~ABCD form
     for term in mask:
-        eqns.append(''.join([("~" if term.term[i] == '0' else "") + chr(ord("A")+i) for i in range(len(term.term)) if term.term[i] != "*"]))
+        string = ""
+        for i in range(len(term.term)):
+            if term.term[i] != "*":
+                not_sym = ("~" if ((term.term[i] == '0') ^ (pos)) else "")
+                string += not_sym + chr(ord("A")+i)
+        eqns.append(string)
 
     return eqns
